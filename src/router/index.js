78c41f92e5193281cf4ebabe0030b71a4285fc65
wwt5555 <1804085672@qq.com>
2020-10-08 18:59:29 +0800

    订单列表完成

--- src/router/index.js
+++ src/router/index.js
@@ -10,43 +10,47 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+
 ]
 
 const router = new VueRouter({
-  routes:[
-    { path: '/', redirect: '/login'},
-    { path: '/login', component:Login },
-    { path: '/home', component:Home, 
-      redirect: '/welcome',
-      children: [{path: '/welcome', component:Welcome},
-      {path: '/users',component:Users},
-      {path: '/rights',component:Rights},
-      {path: '/roles',component:Roles},
-      {path: '/categories',component:Cate},
-      {path: '/params',component:Params},
-      {path: '/goods',component:List},
-      {path: '/goods/add',component:Add}
-    ]}
-  ]
+    routes: [
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        {
+            path: '/home', component: Home,
+            redirect: '/welcome',
+            children: [{ path: '/welcome', component: Welcome },
+            { path: '/users', component: Users },
+            { path: '/rights', component: Rights },
+            { path: '/roles', component: Roles },
+            { path: '/categories', component: Cate },
+            { path: '/params', component: Params },
+            { path: '/goods', component: List },
+            { path: '/goods/add', component: Add },
+            { path: '/orders', component: Order }
+            ]
+        }
+    ]
 })
 
 //挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
-  //to 将要访问的路径
-  //from 代表从哪个路径跳转而来
-  //next 是一个函数，表示放行
-  // next() 放行   next('/login') 强制跳转到登录界面
+router.beforeEach((to, from, next) => {
+    //to 将要访问的路径
+    //from 代表从哪个路径跳转而来
+    //next 是一个函数，表示放行
+    // next() 放行   next('/login') 强制跳转到登录界面
 
-  if(to.path === '/login') return next();
-  //获取token
-  const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
-  next()
+    if (to.path === '/login') return next();
+    //获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
 })
 
 export default router
